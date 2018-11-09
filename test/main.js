a7127eea4c8768ee68a2375212b601a6cbfa1696
PennySeamer <38350380+PennySeamer@users.noreply.github.com>
2018-11-09 18:48:47 +0930

    added json file
    js and json files

---
+++ test/main.js
@@ -0,0 +1,34 @@
+//get json data for suburbs
+var myList = document.querySelector('listSubs');
+
+fetch('parks.json')
+.then(function(response) { return response.json(); })
+.then(function(json) {
+
+  for(var i = 0; i < json.parks.length; i++) {
+    var listItem = document.createElement('li');
+    listItem.innerHTML +=json.parks[i].Location;
+    myList.appendChild(listItem);
+  }
+});
+//get links to pages
+
+var myList = document.querySelector('listSubs');
+
+fetch('parks.json')
+.then(function(response) { return response.json(); })
+.then(function(json) {
+
+  for(var i = 0; i < json.parks.length; i++) {
+    var listItem = document.createElement('li');
+    listItem.innerHTML +=json.parks[i].Name;
+    listItem.innerHTML +=json.parks[i].features;
+    myList.appendChild(listItem);
+  }
+});
+
+document.getElementById("bellamack").onclick = displayBell;
+
+    listItem.innerHTML +=json.parks[i].Name;
+    listItem.innerHTML +=json.parks[i].features;
+    displayBell.appendChild(listItem);
