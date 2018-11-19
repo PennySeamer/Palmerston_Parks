c14d9d7b3d6a86d052a192da3c7cccc537583ec7
PennySeamer <38350380+PennySeamer@users.noreply.github.com>
2018-11-19 12:38:47 +0930

    Update main.js
    try again

--- js/main.js
+++ js/main.js
@@ -26,14 +26,13 @@ fetch('parks.json')
     listItem.innerHTML +=json.parks[i].features;
     myList.appendChild(listItem);
   }
-});
+})
 
-document.getElementById("bellamack").onclick = displayBell;
-
-    listItem.innerHTML +=json.parks[i].Name;
-    listItem.innerHTML +=json.parks[i].features;
-    displayBell.appendChild(listItem);
-
+var ourRequest = new XMLHttpRequest();
+ourRequest.open ("GET", 'https://github.com/PennySeamer/Palmerston_Parks/blob/master/js/parks.json')
+ourRequest,onload = function(){
+  console.log(ourRequest.responseText);
+};
 function createHTML(parksData){
   var rawTemplate = document.getElementById("parksTemplate").innerHTML;
   var compiledTemplate = Handlebars.compile(rawTemplate);
