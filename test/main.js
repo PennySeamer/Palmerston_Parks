ec75399aed7d393f4a30a86a21d7898cd10d585f
PennySeamer <38350380+PennySeamer@users.noreply.github.com>
2018-11-20 13:25:58 +0930

    remove old files
    Deleted all old html/js/css files
    Added maps to json data
    getting data with handlebars tempate

--- test/main.js
+++ test/main.js
@@ -1,41 +1,34 @@
-//get json data for suburbs
-var myList = document.querySelector('listSubs');
-
-fetch('parks.json')
-.then(function(response) { return response.json(); })
-.then(function(json) {
-
-  for(var i = 0; i < json.parks.length; i++) {
-    var listItem = document.createElement('li');
-    listItem.innerHTML +=json.parks[i].Location;
-    myList.appendChild(listItem);
-  }
-});
-//get links to pages
-
-var myList = document.querySelector('listSubs');
-
-fetch('parks.json')
-.then(function(response) { return response.json(); })
-.then(function(json) {
-
-  for(var i = 0; i < json.parks.length; i++) {
-    var listItem = document.createElement('li');
-    listItem.innerHTML +=json.parks[i].Name;
-    listItem.innerHTML +=json.parks[i].features;
-    myList.appendChild(listItem);
-  }
-});
-
-document.getElementById("bellamack").onclick = displayBell;
-
-    listItem.innerHTML +=json.parks[i].Name;
-    listItem.innerHTML +=json.parks[i].features;
-    displayBell.appendChild(listItem);
-
-
 var ourRequest = new XMLHttpRequest();
-ourRequest.open ("GET", 'https://github.com/PennySeamer/Palmerston_Parks/blob/master/js/parks.json')
-ourRequest,onload = function(){
-  console.log(ourRequest.responseText);
+ourRequest.open('GET', 'parks.json');
+ourRequest.onload = function() {
+  if (ourRequest.status >= 200 && ourRequest.status < 400) {
+    var parks = JSON.parse(ourRequest.responseText);
+    createHTML(parks);
+  } else {
+    console.log("We connected to the server, but it returned an error.");
+  }
 };
+
+ourRequest.onerror = function() {
+  console.log("Connection error");
+};
+
+ourRequest.send();
+
+function createHTML(parksTemplate){
+  var rawTemplate = document.getElementById('parksTemplate').innerHTML;
+  var compiledTemplate = Handlebars.compile(rawTemplate);
+  var ourGeneratedHTML = compiledTemplate(parksTemplate);
+
+  var parksContainer =document.getElementById("park1");
+  park1.innerHTML = ourGeneratedHTML;
+}
+
+
+//  for(var i = 0; i < json.parks.length; i++) {
+  //  var listItem = document.createElement('li');
+    //listItem.innerHTML +=json.parks[i].Name;
+    //listItem.innerHTML +=json.parks[i].features;
+  //  myList.appendChild(listItem);
+  //}
+//});
